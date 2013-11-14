d5351797b5120d3b1f95538240a3f1a926e3a153
Martin Bagge / brother <brother@bsnet.se>
2013-11-14 09:59:56 +0000

    New files added from LXDE Pootle server based on templates

---
+++ src/translations/is.ts
@@ -0,0 +1,488 @@
+<!DOCTYPE TS>
+<TS version="2.0" language="is">
+<context>
+    <name>Fm::FontButton</name>
+    <message>
+        <source>Bold</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Italic</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>MainDialog</name>
+    <message>
+        <source>Mouse</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Window Manager Preferences</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Theme</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Appearance</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Font</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Windows</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Move &amp; Resize</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Desktops</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Margins</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Dock</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Window Titles</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Button order:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Moving and Resizing Windows</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Information Dialog</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Focusing Windows</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Focus windows when the mouse pointer moves over them</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Titlebar</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Desktop Margins</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Desktop margins are reserved areas on the edge of your screen.  New windows will not be placed within a margin, and maximized windows will not cover them.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Left:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>px</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Top:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Right:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Bottom:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>All monitors</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>The active monitor</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>The monitor with the mouse</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Primary Monitor</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>The primary monitor is where Openbox will place dialogs, such as the one used for cycling windows.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Fixed Monitor</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Active Monitor</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Monitor With Mouse Pointer</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Centered</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>From left edge</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>From right edge</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>From top edge</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>From bottom edge</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>When resizing terminal windows</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Always</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Never</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Centered on the window</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Above the window</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Fixed position on screen</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Maximizes the window</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Shades the window</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>N: Window icon
+L: Window label (Title)
+I: Iconify (Minimize)
+M: Maximize
+C: Close
+S: Shade (Roll up)
+D: Omnipresent (On all desktops)</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Primary monitor</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>&amp;Install a new theme...</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Create a theme &amp;archive (.obt)...</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>&amp;Windows retain a border when undecorated</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>A&amp;nimate iconify and restore</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>&amp;Active window title: </source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>&amp;Inactive window title: </source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Menu &amp;header: </source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>&amp;Menu Item: </source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Active &amp;On-screen display: </source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Inactive O&amp;n-screen display: </source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Focus &amp;new windows when they appear</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>&amp;Place new windows under the mouse pointer</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>&amp;Center new windows when they are placed</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Prefer to place new windows &amp;on:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Primary &amp;monitor:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>&amp;Fixed monitor:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Update the window contents while &amp;resizing</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Drag &amp;threshold distance:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Amount of resistance against other &amp;windows:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Amount of resistance against screen &amp;edges:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>&amp;Amount of time to wait before switching:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>&amp;Switch desktops when moving a window past the screen edge</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Fixed &amp;y position:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Information dialog's &amp;position:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Fixed &amp;x position:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Show &amp;information dialog:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>&amp;Delay before focusing and raising windows:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Move focus under the mouse when &amp;switching desktops</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Move focus &amp;under the mouse when the mouse is not moving</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>&amp;Raise windows when the mouse pointer moves over them</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Double click on the &amp;titlebar:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Double click ti&amp;me:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>&amp;Show a notification when switching desktops</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>&amp;Amount of time to show the notification for:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>&amp;Number of desktops: </source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>&amp;Desktop names:</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>Obconf::MainDialog</name>
+    <message>
+        <source>Custom actions</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>(Unnamed desktop)</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>About ObConf-Qt</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>A preferences manager for Openbox
+
+Copyright (c) 2003-2013
+
+Authors:
+* Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;
+* Dana Jansens &lt;danakj@orodu.net&gt;
+* Tim Riley &lt;tr@slackzone.org&gt;
+* Javeed Shaikh &lt;syscrash2k@gmail.com&gt;</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Choose an Openbox theme</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>"%1" was installed to %1</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>"%1" was successfully created</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Unable to create the theme archive "%1".
+The following errors were reported:
+%2</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Unable to run the "tar" command: %1</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Unable to create directory "%1": %2</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>"%1" does not appear to be a valid Openbox theme directory</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Unable to move to directory "%1": %2</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Unable to extract the file "%1".
+Please ensure that "%2" is writable and that the file is a valid Openbox theme archive.
+The following errors were reported:
+%3</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>ObConf Error</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Syntax: obconf [options] [ARCHIVE.obt]
+
+Options:
+  --help                Display this help and exit
+  --version             Display the version and exit
+  --install ARCHIVE.obt Install the given theme archive and select it
+  --archive THEME       Create a theme archive from the given theme directory
+  --config-file FILE    Specify the path to the config file to use
+</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>--install requires an argument
+</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>--archive requires an argument
+</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>--config-file requires an argument
+</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Failed to load an rc.xml. You have probably failed to install Openbox properly.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Error while parsing the Openbox configuration file.  Your configuration file is not valid XML.
+
+Message: %1</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Copyright (c) 2003-2008   Dana Jansens
+Copyright (c) 2003        Tim Riley
+Copyright (c) 2007        Javeed Shaikh
+Copyright (c) 2013        Hong Jen Yee (PCMan)
+
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions. See the file COPYING for details.
+
+</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
