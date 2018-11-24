d8a782cecc1eb8521528608489d90877a22177c9
Einar Mostad <einar@mostad.eu>
2018-11-24 22:35:28 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (134 of 134 strings)
    
    Translation: LXQt/ObConf-Qt
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/obconf-qt/nb_NO/

--- src/translations/obconf-qt_nb_NO.ts
+++ src/translations/obconf-qt_nb_NO.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../fontbutton.cpp" line="46"/>
         <source>Bold</source>
-        <translation></translation>
+        <translation>Fet</translation>
     </message>
     <message>
         <location filename="../fontbutton.cpp" line="50"/>
         <source>Italic</source>
-        <translation></translation>
+        <translation>Kursiv</translation>
     </message>
 </context>
 <context>
@@ -575,7 +575,7 @@ D: Omnipresent (On all desktops)</source>
     <message>
         <location filename="../obconf.ui" line="1306"/>
         <source>About</source>
-        <translation></translation>
+        <translation>Om</translation>
     </message>
 </context>
 <context>
