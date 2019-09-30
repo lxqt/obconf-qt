cdd752e2207d9fa6cf8d99df311198b45d8dc8bd
scootergrisen <scootergrisen@gmail.com>
2019-09-30 20:48:42 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (134 of 134 strings)
    
    Translation: LXQt/ObConf-Qt
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/obconf-qt/da/

--- src/translations/obconf-qt_da.ts
+++ src/translations/obconf-qt_da.ts
@@ -639,7 +639,7 @@ Programmet er baseret på ObConf udviklet af følgende udviklere.
     <message>
         <location filename="../archive.cpp" line="26"/>
         <source>&quot;%1&quot; was installed to %2</source>
-        <translation type="unfinished">&quot;%2&quot; blev installeret i %2 {1&quot;?}</translation>
+        <translation>&quot;%1&quot; blev installeret på %2</translation>
     </message>
     <message>
         <location filename="../archive.cpp" line="53"/>
