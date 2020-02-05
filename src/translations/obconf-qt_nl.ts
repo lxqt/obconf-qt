920df1a65478008784a21d0a9c172b10b915de01
Pander <pander@users.sourceforge.net>
2020-02-05 21:06:55 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (134 of 134 strings)
    
    Translation: LXQt/ObConf-Qt
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/obconf-qt/nl/

--- src/translations/obconf-qt_nl.ts
+++ src/translations/obconf-qt_nl.ts
@@ -639,7 +639,7 @@ Het programma is gebaseerd op ObConf, ontwikkeld door:
     <message>
         <location filename="../archive.cpp" line="26"/>
         <source>&quot;%1&quot; was installed to %2</source>
-        <translation type="unfinished">&quot;%2&quot; werd geïnstalleerd op %2 {1&quot;?}</translation>
+        <translation>&quot;%1&quot; was geïnstalleerd in %2</translation>
     </message>
     <message>
         <location filename="../archive.cpp" line="53"/>
