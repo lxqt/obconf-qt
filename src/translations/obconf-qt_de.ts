d10aa9dcb23c9cb02f8abc34fdd6abdb2af5a61a
Weblate <noreply@weblate.org>
2024-08-30 12:58:53 +0200

    Weblate commits
    Co-authored-by: Ali Alhaidary علي الحيدري <ali.alhaidary@the5stars.org>
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Co-authored-by: Jan Rolski <wbcwknvstb@proton.me>
    Co-authored-by: Mincho Kondarev <mkondarev@yahoo.de>
    Co-authored-by: Moo <hazap@hotmail.com>
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Co-authored-by: Standreas <standreas@riseup.net>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/obconf-qt/ar/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/obconf-qt/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/obconf-qt/de/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/obconf-qt/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/obconf-qt/it/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/obconf-qt/lt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/obconf-qt/nl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/obconf-qt/pl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/obconf-qt/uk/
    Translation: LXQt Configuration/obconf-qt

--- src/translations/obconf-qt_de.ts
+++ src/translations/obconf-qt_de.ts
@@ -758,13 +758,14 @@ Optionen:
     <message>
         <location filename="../obconf-qt.cpp" line="206"/>
         <source>Platform Unsupported</source>
-        <translation type="unfinished"></translation>
+        <translation>Plattform nicht unterstützt</translation>
     </message>
     <message>
         <location filename="../obconf-qt.cpp" line="207"/>
         <source>ObConf-Qt is unsupported under Wayland.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>ObConf-Qt kann nicht benutzt werden in Wayland.
+</translation>
     </message>
     <message>
         <location filename="../obconf-qt.cpp" line="254"/>
--- src/translations/obconf-qt_nl.ts
+++ src/translations/obconf-qt_nl.ts
@@ -754,13 +754,14 @@ Opties:
     <message>
         <location filename="../obconf-qt.cpp" line="206"/>
         <source>Platform Unsupported</source>
-        <translation type="unfinished"></translation>
+        <translation>Geen ondersteund platform</translation>
     </message>
     <message>
         <location filename="../obconf-qt.cpp" line="207"/>
         <source>ObConf-Qt is unsupported under Wayland.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>ObConf-Qt wordt niet ondersteund op Wayland.
+</translation>
     </message>
     <message>
         <location filename="../obconf-qt.cpp" line="254"/>
