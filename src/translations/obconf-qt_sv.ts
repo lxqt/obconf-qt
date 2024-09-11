8c4f41a0475f7701234ef300fcc6a767055d0f6a
Weblate <noreply@weblate.org>
2024-09-11 09:17:48 +0200

    Weblate commits
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Co-authored-by: bittin <bittin@reimu.nl>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/obconf-qt/nl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/obconf-qt/sv/
    Translation: LXQt Configuration/obconf-qt

--- src/translations/obconf-qt_sv.ts
+++ src/translations/obconf-qt_sv.ts
@@ -732,13 +732,14 @@ Flaggor:
     <message>
         <location filename="../obconf-qt.cpp" line="206"/>
         <source>Platform Unsupported</source>
-        <translation type="unfinished"></translation>
+        <translation>Plattform stöds ej</translation>
     </message>
     <message>
         <location filename="../obconf-qt.cpp" line="207"/>
         <source>ObConf-Qt is unsupported under Wayland.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>ObConf-Qt stöds inte under Wayland.
+</translation>
     </message>
     <message>
         <location filename="../obconf-qt.cpp" line="241"/>
