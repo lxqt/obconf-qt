caac2827f58f94a23c8e1db5899eae75a8f67c30
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2024-10-23 15:35:49 +0200

    Weblate commits (#242)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/obconf-qt/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/obconf-qt/nb_NO/
    Translation: LXQt Configuration/obconf-qt
    
    Co-authored-by: Jouni Järvinen <jounijarvis@gmail.com>
    Co-authored-by: ovl-1 <ovl-002@mailinator.com>

--- src/translations/obconf-qt_fi.ts
+++ src/translations/obconf-qt_fi.ts
@@ -755,13 +755,14 @@ Vivut:
     <message>
         <location filename="../obconf-qt.cpp" line="206"/>
         <source>Platform Unsupported</source>
-        <translation type="unfinished"></translation>
+        <translation>Alustaa ei tueta</translation>
     </message>
     <message>
         <location filename="../obconf-qt.cpp" line="207"/>
         <source>ObConf-Qt is unsupported under Wayland.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>ObConf-Qt ei tue Waylandiä.
+</translation>
     </message>
     <message>
         <location filename="../obconf-qt.cpp" line="241"/>
