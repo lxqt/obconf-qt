b50c477bdd745e70e5d49992f696087f85016d85
Weblate <noreply@weblate.org>
2025-06-29 09:26:57 +0200

    Weblate commits
    Co-authored-by: Simon Lindén <simon_l91@hotmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: therealmate <hellogaming91@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/obconf-qt-menu-entry/sv/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/obconf-qt/hu/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/obconf-qt/sv/
    Translation: LXQt Configuration/obconf-qt
    Translation: LXQt Configuration/obconf-qt Menu Entry

--- src/translations/obconf-qt_hu.ts
+++ src/translations/obconf-qt_hu.ts
@@ -755,13 +755,14 @@ Opciók:
     <message>
         <location filename="../obconf-qt.cpp" line="206"/>
         <source>Platform Unsupported</source>
-        <translation type="unfinished"></translation>
+        <translation>Nem támogatott platform</translation>
     </message>
     <message>
         <location filename="../obconf-qt.cpp" line="207"/>
         <source>ObConf-Qt is unsupported under Wayland.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Az ObConf-Qt nem támogatott Waylandon
+</translation>
     </message>
     <message>
         <location filename="../obconf-qt.cpp" line="241"/>
